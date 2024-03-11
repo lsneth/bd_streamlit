148798a9d5156766f22450ebd3ac346f60450ac3
Luke Nethercott <luke+github@nethercott.com>
2024-03-11 17:46:59 -0600

    work on charts page

--- pages/1_Charts.py
+++ pages/1_Charts.py
@@ -4,40 +4,47 @@ import altair as alt
 
 st.set_page_config(page_title="Charts")
 
-df = pl.read_parquet('./data/survey_results_public.parquet')
-
-
-st.title('Explore Page')
+st.title('[Charts](https://docs.streamlit.io/library/api-reference/charts)')
 
 st.header('Stack Overflow Developer Survey Data')
-st.dataframe(df.head())
 
-test = df\
-    .drop_nulls(subset=['YearsCode', 'YearsCodePro', 'ConvertedCompYearly'])\
+df = pl.read_parquet('./data/survey_results_public.parquet')\
+    .drop_nulls(subset=['YearsCodePro', 'ConvertedCompYearly'])\
     .with_columns(
         pl.col('YearsCode').replace({'Less than 1 year': 0, 'More than 50 years': 50}).cast(pl.Int32),
         pl.col('YearsCodePro').replace({'Less than 1 year': 0, 'More than 50 years': 50}).cast(pl.Int32),
     )\
-    .with_columns((pl.col('YearsCode') - pl.col('YearsCodePro')).alias('YearsCodeNonPro'))\
-    .select(pl.col('YearsCode'), pl.col('YearsCodePro'), pl.col('YearsCodeNonPro'), pl.col('ConvertedCompYearly'))\
-    .filter(pl.col('YearsCodeNonPro') > 0)\
-    .filter(pl.col('YearsCodePro') > 0)\
-    .filter(pl.col('YearsCode') > 0)\
     .sort('YearsCodePro')
-    
-st.header('Developer Count By Number of Years of Professional Experience')
-st.line_chart(test.group_by('YearsCodePro').count(), x='YearsCodePro', y='count')
 
-st.header('Salary of 10 Year Pros by Number of Nonpro Years of Experience')
-st.scatter_chart(test.filter(pl.col('YearsCodePro') == 10), x='ConvertedCompYearly', y='YearsCodeNonPro', size=5)
+st.dataframe(df.head())
 
-st.header('Salary of 4 Year Pros by Number of Nonpro Years of Experience')
-st.scatter_chart(test.filter(pl.col('YearsCodePro') == 4), x='ConvertedCompYearly', y='YearsCodeNonPro', size=5)
+st.header('Salary by Number of Years of Professional Experience - [`st.scatter_chart`](https://docs.streamlit.io/library/api-reference/charts/st.scatter_chart)')
+st.scatter_chart(df, x='YearsCodePro', y='ConvertedCompYearly', size=5)
 
-chart = alt.Chart(test.filter(pl.col('YearsCodePro') == 4)).mark_circle(size=5).encode(
-    x=alt.X('ConvertedCompYearly', scale=alt.Scale(domain=(0, 200000))),
-    y=alt.Y('YearsCodeNonPro', scale=alt.Scale(domain=(0, 10))),
-    tooltip=['ConvertedCompYearly', 'YearsCodeNonPro']
+st.header('Salary by Number of Years of Professional Experience - [`st.altair_chart`](https://docs.streamlit.io/library/api-reference/charts/st.altair_chart)')
+chart = alt.Chart(df).mark_circle(size=5).encode(
+    x='YearsCodePro',
+    y=alt.Y('ConvertedCompYearly', scale=alt.Scale(domain=(0, 300000))),
+    tooltip=['YearsCodePro', 'ConvertedCompYearly']
 ).interactive()
-
 st.altair_chart(chart, use_container_width=True)
+
+st.write('''
+---
+         
+### Altair Wrappers
+- `st.area_chart`
+- `st.bar_chart`
+- `st.line_chart`
+- `st.scatter_chart`
+         
+### Library Integrations
+- `st.pyplot`
+- `st.altair_chart`
+- `st.vega_lite_chart`
+- `st.plotly_chart`
+- `st.bokeh_chart`
+- `st.pydeck_chart`
+- `st.graphviz_chart`
+- `st.map`
+''')
